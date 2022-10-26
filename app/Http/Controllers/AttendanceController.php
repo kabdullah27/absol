868c109be2af21cd1c1578b37161b5022bb11b1b
Khalid Abdullah <khalid@dagangan.com>
2022-10-26 15:11:10 +0700

    complete feature checkin and checkout

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,31 +6,85 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\TxAttendance;
+use App\Models\MtSchedule;
+use Carbon\Carbon;
 
 class AttendanceController extends Controller
 {
-    public function index():View
+    public function index(): View
     {
-        // dd(auth()->user());
-        $datas = \App\Models\TxAttendance::with('user', 'schedule', 'location_checkin','location_checkout', 'user.employee', 'schedule.shift')->get();
-        return view('attendance.index', compact('datas'));
+        $datas = TxAttendance::with('user', 'schedule', 'location_checkin', 'location_checkout', 'user.employee', 'schedule.shift')
+            ->whereDate('attendance_date_check_in', '=', date('Y-m-d'))
+            ->get();
+
+        $filter_user = $datas->filter(function ($item) {
+            return $item->user_id == auth()->user()->id;
+        })->first();
+
+        $title = (!$filter_user) ? 'Checkin' : 'Checkout';
+
+        return view('attendance.index', compact('datas', 'title'));
     }
 
-    public function create()
+    public function create(): View
     {
         $user = auth()->user();
 
-        $title = 'Checkin';
+        $datas = TxAttendance::whereDate('attendance_date_check_in', '=', date('Y-m-d'))
+            ->where('user_id', $user->id)
+            ->first();
+
+        $title = (!$datas == 0) ? 'Checkin' : 'Checkout';
 
         return view('attendance.create', compact('user', 'title'));
     }
 
     public function store(Request $request): RedirectResponse
     {
-        /** @var \App\Models\User $user */
         $user = auth()->user();
-        dd($request);
+        $data_attendance = TxAttendance::whereDate('attendance_date_check_in', '=', date('Y-m-d'))
+            ->where('user_id', $user->id)
+            ->first();
 
-        return redirect()->back()->withSuccess(__('Berhasil Checkin') ?? '');
+        $title = (!$data_attendance) ? 'Checkin' : 'Checkout';
+        
+        $date_schedule = MtSchedule::whereDate('schedule_date', '=', date('Y-m-d'))
+            ->where('user_id', $user->id)
+            ->first();
+            
+            DB::beginTransaction();
+        try {
+            if($data_attendance){
+                $attendance = TxAttendance::find($data_attendance->id);
+                $attendance->location_id_check_out = $request->location;
+                $attendance->attendance_desc_check_out = $request->notes;
+                $attendance->attendance_date_check_out = Carbon::now()->toDateTimeString();
+                $attendance->attendance_long_check_out = $request->long;
+                $attendance->attendance_lat_check_out = $request->lat;
+                $attendance->updated_by = $user->id;
+                $attendance->updated_at = Carbon::now()->toDateTimeString();
+                $attendance->save();
+            }else{
+                $attendance = new TxAttendance;
+                $attendance->user_id = $user->id;
+                $attendance->schedule_id = $date_schedule->id;
+                $attendance->location_id_check_in = $request->location;
+                $attendance->attendance_desc_check_in = $request->notes;
+                $attendance->attendance_date_check_in = Carbon::now()->toDateTimeString();
+                $attendance->attendance_long_check_in = $request->long;
+                $attendance->attendance_lat_check_in = $request->lat;
+                $attendance->created_by = $user->id;
+                $attendance->updated_by = $user->id;
+                $attendance->save();
+            }
+
+            DB::commit();
+            return redirect()->back()->withSuccess(__('Berhasil'.$title) ?? '');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->back()->withErrors(__('Tidak Berhasil'.$title) ?? '');
+        }
     }
 }
--- resources/views/attendance/create.blade.php
+++ resources/views/attendance/create.blade.php
@@ -5,11 +5,37 @@
         @php
             $query = 'SELECT id, location_name from mt_locations order by location_name';
         @endphp
-        
+
         {!! form()->dropdownDB('location', $query, $keyColumn = 'id', $valueColumn = 'location_name')->label('location') !!}
-        {!! form()->textarea('notes')->label('Notes'); !!}
+        {!! form()->textarea('notes')->label('Notes') !!}
+        {!! form()->hidden('lat')->id('lat') !!}
+        {!! form()->hidden('long')->id('long') !!}
 
         {!! form()->action(form()->submit('Save')) !!}
         {!! form()->close() !!}
     </x-volt-panel>
 </x-volt-app>
+
+
+<script>
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        const crd = pos.coords;
+
+        document.getElementById("lat").value = crd.latitude;
+        document.getElementById("long").value = crd.longitude;
+
+    }
+
+    function error(err) {
+        alert(`ERROR: ${err.message}`);
+        window.location.href = `${document.location.origin}/attendance`;
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+</script>
